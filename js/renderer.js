ddee75d499a8e9f38f4021b4f82a971e9361c4bc
Andrea Rubino <andrea.rubino@pronetics.it>
2017-11-24 17:31:10 +0100

    BEST COMMIT MESSAGE EU EVER
    YOYO

--- js/renderer.js
+++ js/renderer.js
@@ -1,4 +1,4 @@
-gInitialMoney = 1000;
+gInitialMoney = 5000;
 gMoney = gInitialMoney;
 gIsGameOver = false;
 
@@ -26,15 +26,42 @@ function syncMoneyUI()
     money.innerHTML = "Money: " + gMoney;
 }
 
+function gameloop(){
+	if (gIsGameOver)
+	{
+		return;
+	}
+	syncMoneyUI();
+	checkGameOver();
+}
+
 function onPayTaxes()
 {
 	if (gIsGameOver)
 	{
 		return;
 	}
-	let t = Math.floor(gInitialMoney + 2000 * Math.random());
+	let t = Math.floor(gInitialMoney*0.9 * Math.random());
 	_gText.appendChild(append("You have paid " + t + " in taxes"));
 	gMoney -= t;
-	syncMoneyUI();
-	checkGameOver();
+	gameloop();
 }
+
+function onWork(){
+	if (gIsGameOver)
+	{
+		return;
+	}
+	let wage = Math.floor(1000 * Math.random());
+	_gText.appendChild(append("You have gained " + wage + " $."));
+	gMoney += wage;
+	gameloop();
+}
+
+$( document ).ready(function() {
+	console.log( "Starting loop" );
+	setInterval(function(){ 
+		onPayTaxes();
+		console.log( "tt" );
+	}, 1000);
+});
