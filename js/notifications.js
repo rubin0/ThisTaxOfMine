f739a03adcbdc60e7cae4974c287008f3a00510f
Andrea Rubino <andrea.rubino@pronetics.it>
2017-11-26 13:51:14 +0100

    MORE OBJECTIVIFICATIONS BOYS

--- js/notifications.js
+++ js/notifications.js
@@ -1,31 +1,27 @@
 var Notifications = {
-    GAIN_CSS : "award",
-    LOSS_CSS : "cost",
-    MAX_NOTIFICATIONS_NUMBER : 10,
+    GAIN_CSS: "award",
+    LOSS_CSS: "cost",
+    MAX_NOTIFICATIONS_NUMBER: 15,
 
-    init : function(){
-
+    init: function () {
     },
-    div : document.getElementById("text"),
-    create : function(message, value){
+    parentDiv: document.getElementById("text"),
+    create: function (message, value) {
         let string = document.createElement('p');
         string.innerHTML = message;
 
-        if(value >0){
+        if (value > 0) {
             string.className += "award";
-        } else if(value < 0){
+        } else if (value < 0) {
             string.className += "cost";
         }
 
-        Notifications.div.appendChild(string);
-
-        console.log(string);
-
-        Notifications.removeOldNotifications();
+        this.parentDiv.appendChild(string);
+        this.removeOldNotifications();
     },
-    removeOldNotifications : function(){
-        let notifications = Notifications.div.children;
-        if(notifications.length > Notifications.MAX_NOTIFICATIONS_NUMBER){
+    removeOldNotifications: function () {
+        let notifications = this.parentDiv.children;
+        if (notifications.length > this.MAX_NOTIFICATIONS_NUMBER) {
             notifications[0].remove();
         }
     }
--- js/renderer.js
+++ js/renderer.js
@@ -1,15 +1,11 @@
-gInitialMoney = 5000;
-gMoney = gInitialMoney;
 gIsGameOver = false;
 gRefreshInterval = null;
 
 let _gText = document.getElementById("text");
 let money = document.getElementById("money");
 
-function checkGameOver()
-{
-	if (gMoney <= 0)
-	{
+function checkGameOver() {
+	if (World.money <= 0) {
 		clearInterval(gRefreshInterval);
 		gIsGameOver = true;
 		Notifications.create("GAME OVER :(");
@@ -17,64 +13,49 @@ function checkGameOver()
 	}
 }
 
-function syncMoneyUI()
-{
-    money.innerHTML = "Money: " + gMoney;
-}
-
-function gameloop()
-{
-	if (gIsGameOver)
-	{
+function gameloop() {
+	if (gIsGameOver) {
 		return;
 	}
-	syncMoneyUI();
 	checkGameOver();
 }
 
-function onPayTaxes()
-{
-	if (gIsGameOver)
-	{
+function onPayTaxes() {
+	if (gIsGameOver) {
 		return;
 	}
-	let t = -Math.floor(gInitialMoney*0.9 * Math.random());
+	let t = -Math.floor((World.money * 0.9 * Math.random()+50))	;
 	Notifications.create("You have paid " + t + " in taxes", t);
-	gMoney += t;
+	World.setMoney(t);
 	gameloop();
 }
 
-function onWork()
-{
-	if (gIsGameOver)
-	{
+function onWork() {
+	if (gIsGameOver) {
 		return;
 	}
 	let wage = Math.floor(1000 * Math.random());
 	Notifications.create("You have gained " + wage, wage);
-	console.log(_gText);
-	gMoney += wage;
+	World.setMoney(wage);
 	gameloop();
 }
 
-function onRestart()
-{
-	if (gIsGameOver)
-	{
+function onRestart() {
+	if (gIsGameOver) {
 		_gText.innerHTML = '<p id="start">You have to pay taxes.</p>';
-		gMoney = gInitialMoney;
 		$("#bnRestart").hide();
 		gIsGameOver = false;
-		syncMoneyUI();
-		gRefreshInterval = setInterval(function(){
+		World.init();
+		gRefreshInterval = setInterval(function () {
 			onPayTaxes();
 		}, 1000);
 	}
 }
 
-$( document ).ready(function() {
+$(document).ready(function () {
+	World.init();
 	//console.log( "Starting loop" );
-	gRefreshInterval = setInterval(function(){ 
+	gRefreshInterval = setInterval(function () {
 		onPayTaxes();
 	}, 1000);
 });
