ea9bb2cb9ddf634bcd1feb03ab741dc3c5a5359b
Andrea Rubino <andrea.rubino@pronetics.it>
2017-11-26 16:01:34 +0100

    countdown

--- js/renderer.js
+++ js/renderer.js
@@ -5,6 +5,7 @@ function onPayTaxes() {
 	let t = -Math.floor((World.money * 0.9 * Math.random() + 50));
 	Notifications.create("You have paid " + Math.abs(t) + " in taxes", t);
 	World.setMoney(t);
+
 }
 
 function onWork() {
@@ -14,6 +15,7 @@ function onWork() {
 	let wage = Math.floor(1000 * Math.random());
 	Notifications.create("You have gained " + wage, wage);
 	World.setMoney(wage);
+	World.syncUI();
 }
 
 function onRestart() {
--- js/world.js
+++ js/world.js
@@ -1,16 +1,20 @@
 var World = {
+    DAYS_TO_PAY: 10,
     money: 0,
+    gametime: 0,
     gRefreshInterval: null,
     gIsGameOver: false,
     gRefreshInterval: null,
     moneyDiv: document.getElementById("money"),
+    timeDiv: document.getElementById("time"),
     init: function () {
         $("#bnRestart").hide();
 
+        this.gametime = this.DAYS_TO_PAY;
         Notifications.init();
 
         this.setMoney(5000);
-        this.syncMoneyUI();
+        this.syncUI();
 
         this.gIsGameOver = false;
 
@@ -20,17 +24,29 @@ var World = {
     },
     setMoney: function (amount) {
         this.money += amount;
-        this.syncMoneyUI();
     },
-    syncMoneyUI: function () {
+    syncUI: function () {
         this.moneyDiv.innerText = "Money: " + this.money;
+        this.timeDiv.innerText = this.gametime;
+        if(this.timeDiv.innerText >= 0 && this.timeDiv.innerText <= 5){
+            this.timeDiv.className = "countdown";
+        } else {
+            this.timeDiv.className = "";
+        }
     },
     gameloop: function () {
-        onPayTaxes();
+        this.gametime -= 1;
+
+        if (this.gametime < 0) {
+            onPayTaxes();
+            this.gametime = this.DAYS_TO_PAY;
+        }
+
         this.checkGameOver();
         if (this.gIsGameOver) {
             return;
         }
+        this.syncUI();
     },
     checkGameOver: function () {
         if (World.money <= 0) {
