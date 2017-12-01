3241b3e726d2b0c8e3c123259a9ce39fc30a5825
Andrea Rubino <andrea.rubino@pronetics.it>
2017-12-01 16:19:33 +0100

    Qualche fix e un po' di refactoring che male non fa

--- js/world.js
+++ js/world.js
@@ -11,8 +11,6 @@ var World = {
     timeDiv: document.getElementById('time'),
     scoreDiv: document.getElementById('scoretime'),
     init: function () {
-        $('#bnRestart').hide();
-
         this.gametime = this.DAYS_TO_PAY;
         this.score = 0;
         Notifications.init();
@@ -20,9 +18,9 @@ var World = {
         this.setMoney(5000);
         this.syncUI();
 
-        this.gIsGameOver = false; 
+        this.gIsGameOver = false;
     },
-    start: function (){
+    start: function () {
         this.gRefreshInterval = setInterval(function () {
             World.gameloop();
         }, 1000);
@@ -37,7 +35,7 @@ var World = {
         this.moneyDiv.innerText = 'Money: ' + this.money;
         this.timeDiv.innerText = this.gametime;
         this.scoreDiv.innerText = this.score;
-        if(this.timeDiv.innerText >= 0 && this.timeDiv.innerText <= 5){
+        if (this.timeDiv.innerText >= 0 && this.timeDiv.innerText <= 5) {
             this.timeDiv.className = 'countdown';
         } else {
             this.timeDiv.className = '';
@@ -47,7 +45,7 @@ var World = {
         this.gametime -= 1;
 
         if (this.gametime < 0) {
-            onPayTaxes();
+            this.onPayTaxes();
             this.gametime = this.DAYS_TO_PAY;
         }
         this.score++;
@@ -62,7 +60,58 @@ var World = {
             this.gIsGameOver = true;
             clearInterval(this.gRefreshInterval);
             Notifications.create('GAME OVER :(');
-            $('#bnRestart').show();
+            $("#bnRestart").show();
         }
+    },
+    onPayTaxes: function () {
+        if (World.gIsGameOver) {
+            return;
+        }
+        let t = -Math.floor((World.money * 0.9 * Math.random() + 50));
+        Notifications.create("You have paid " + Math.abs(t) + " in taxes", t);
+        World.addMoney(t);
+    },
+    onWork: function () {
+        if (World.gIsGameOver) {
+            return;
+        }
+        let wage = Math.floor(1000 * Math.random());
+        Notifications.create("You have gained " + wage, wage);
+        World.addMoney(wage);
+        World.syncUI();
+    },
+    onRestart: () => {
+        World.saveScore();
+        World.init();
+        World.start();
+    },
+    onStart: () => {
+        if (World.onLogin()) {
+            World.init();
+            World.start();
+        }
+    },
+    onLogin: () => {
+        var username = $("#inputStart").val();
+        if (!(username === "")) {
+            $("#inputStart").hide();
+            $("#bnStart").hide();
+            $("#username").text("Logged as: " + username);
+            $("#username").show();
+            $("#errorUsername").hide();
+            $("#bnWork").show();
+            localStorage.username = username;
+            return true;
+        } else {
+            $("#errorUsername").text("Inserire un username corretto!");
+            $("#errorUsername").show();
+            return false;
+        }
+    },
+    onSaveScore: () => {
+        $.post("https://backend-ttom.herokuapp.com/users", { username: localStorage.username, score: World.score },
+            function (returnedData) {
+                console.log(returnedData);
+            });
     }
 }
